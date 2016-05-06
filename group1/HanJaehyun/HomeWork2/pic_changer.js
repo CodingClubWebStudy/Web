ce2c5151eed89611456f923a496c29650eddea63
HanJaehyun <nugurejeil@naver.com>
2016-05-06 18:49:57 +0900

    과제 자바스크립트 수정

--- group1/HanJaehyun/HomeWork2/pic_changer.js
+++ group1/HanJaehyun/HomeWork2/pic_changer.js
@@ -5,8 +5,6 @@ for(var i=0; i< pic_list.length; i++){
     pic_list[i].onclick = function(event){
         event.preventDefault();
         var pic_href = this.getAttribute('href');
-        console.log(pic_href);
         pic_background.style.backgroundImage= 'url('+pic_href+')';
-//        pic_background.setAttribute('background-image', pic_href);
     }
 }
